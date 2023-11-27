5f7d0efb71acf306b2ba23114ea033e25a363596
Sandeep <mysisneru@123gmail.com>
2023-11-27 11:59:23 +0530

    Controller function has been updated for product adding

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.all("*", (req, res, next) => {
     next(err)
 })
 
+app.get("/", (req, res) =>{
+    res.status(200).json({status: "success", message: "This is a sample api for Fele Fashion"})
+})
+
 // Global error handler
 app.use(errorHandler)
 
